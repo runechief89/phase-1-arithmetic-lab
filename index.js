613065abdda4da97585a5d79e0d6f5d021f81f41
Min <runechief89@gmail.com>
2022-02-25 23:31:44 -0500

    commit tree 2fab0c2a1cd0edf5b06a511de94711734b159b4f

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+let num1 = 1;
+let num2 = 62;
+let num3 = 9;
+let num4 = 5;
+let num5 = 20;
+let multiply;
+let random;
+let mod;
+let max;
+
+multiply = num1 * num2;
+
+random = Math.floor(Math.random() * 100) + 1;
+
+mod = num3 % num4;
+
+max = Math.max( num1, num3, num4, num5);
